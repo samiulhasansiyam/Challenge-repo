ad1928121b5e8c5f04ddc8823e4f0ef0d358e060
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-04-27 23:05:36 +0600

    Add files via upload

--- ICPC/A_Say_Hello_With_C.cpp
+++ ICPC/A_Say_Hello_With_C.cpp
@@ -1,11 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    string hello="Hello, ";
-    string Input;
-    cin>>Input;
-
-    cout<<hello<<Input<< endl;
-    return 0;
+int main() {
+  char H = 72;
+  char e=101;
+  char l=108;
+  char ll=108;
+  char o=111;
+  char c=44 ;
+  char sp=32 ;
+  string s;
+  cin>>s;
+  
+  cout <<H<<e<<l<<l<<o<<c<<sp << s << endl;
+  return 0;
 }
