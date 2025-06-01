39337cb8cd7af3f7c21186d7867046be6053e59a
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-06-01 11:53:42 +0600

    J_Primes_from_1_to_n

---
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #2 (Loops)/J_Primes_from_1_to_n.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int n;
+ cin >> n;
+ for (int i = 2; i <= n; ++i) 
+ {
+ bool is_prime = true;
+    for (int j = 2; j * j <= i; ++j) 
+    {
+    if (i % j == 0) 
+    {
+    is_prime = false;
+    break;
+    }
+    }
+    if (is_prime) 
+    {
+    cout << i << " ";
+    }
+    }
+    cout << endl;
+    return 0;
+}
