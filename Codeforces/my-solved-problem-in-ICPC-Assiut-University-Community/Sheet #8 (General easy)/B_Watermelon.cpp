e30440dcc5dc62ad84facec7ac11ac9aa6d71565
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-06-05 17:05:47 +0600

    /Sheet #8 (General easy)/B_Watermelon

---
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #8 (General easy)/B_Watermelon.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w;
+    if(w%2==1)
+    {
+        cout<<"NO"<< "\n";
+    }else
+    {
+        if(2==w)
+        {
+            cout<<"No"<< "\n";
+        }else
+        {
+            cout<<"YES"<< "\n";
+        }
+    }
+    return 0;
+}
