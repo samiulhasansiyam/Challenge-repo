d58b7114fe1479a9f41722a36f81d1747ee5b30f
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-06-23 21:01:40 +0600

    H_Sorting using swap function

---
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #3 (Arrays)/H_Sorting.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i=0; i<n ; i++)
+    {
+        cin >> a[i];
+    }
+    for(int i=0; i<n ; i++)
+    {
+        for(int j=i+1 ; j<n ; j++)
+        {
+            if (a[j] < a[i])
+            {
+                swap(a[j],a[i]);
+            }
+        }
+    }
+    for(int i=0; i<n ; i++)
+    {
+        cout << a[i] << " " ;
+    }
+    return 0;
+}
+
+    // for(int i=0; i<n ; i++)
+    // {
+    //     for(int j=i+1 ; j<n ; j++)
+    //     {
+    //         if (a[j] < a[i])
+    //         {
+    //             swap(a[j],a[i]);
+    //         }
+    //     }
+    // }
