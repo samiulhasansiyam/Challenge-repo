3e82c6d9f1da8e741961f7d58660282efcd6eea6
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-06-24 17:27:23 +0600

    I. Smallest Pair(M)

--- Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #3 (Arrays)/I_Smallest_Pair.cpp
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #3 (Arrays)/I_Smallest_Pair.cpp
@@ -1,29 +1,60 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int t, keep,sum,d;
+int main() {
+    int t;
     cin >> t;
-    int n;
-    cin >> n;
-    vector<long long int> a(n);
-    for(int i=1;i<=n;i++)
+    while (t--) 
     {
-        cin >> a[i];
-    }
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=i+1;j<=n;j++)
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) 
         {
-            
-            sum=(a[i]+a[j])+(j-i);
-            d=sum;
-            if (keep > d)
+            cin >> a[i];
+        }
+        int smallest = INT_MAX; //max value that  int can contain  
+
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
             {
-                keep = d;
+                int sum = a[i] + a[j] + (j - i);
+                smallest = min(smallest, sum); // function to search small value 
             }
         }
+
+        cout << smallest << "\n";
     }
-    cout << keep ;
+
     return 0;
 }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int t, keep,sum,d;
+//     cin >> t;
+//     int n;
+//     cin >> n;
+//     vector<long long int> a(n);
+//     for(int i=1;i<=n;i++)
+//     {
+//         cin >> a[i];
+//     }
+//     for(int i=1;i<=n;i++)
+//     {
+//         for(int j=i+1;j<=n;j++)
+//         {
+            
+//             sum=(a[i]+a[j])+(j-i);
+//             d=sum;
+//             if (keep > d)
+//             {
+//                 keep = d;
+//             }
+//         }
+//     }
+//     cout << keep ;
+//     return 0;
+// }
