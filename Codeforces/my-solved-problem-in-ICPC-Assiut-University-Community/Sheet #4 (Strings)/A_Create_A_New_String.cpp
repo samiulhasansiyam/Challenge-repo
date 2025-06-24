54781a0bc57fcf7fa6feb8c172494760335fb595
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-06-24 18:27:53 +0600

    A_Create_A_New_String

---
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #4 (Strings)/A_Create_A_New_String.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s , t;
+    cin >> s;
+    cin >> t;
+    cout << strlen(s.c_str()) << " " << strlen(t.c_str()) << "\n";
+    cout << s << " " << t;
+    return 0;
+}
