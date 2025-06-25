165c5471348c77dea0d848badd8b8b89939f9f9c
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-06-25 18:59:08 +0600

    C_Compare

--- Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #4 (Strings)/C_Compare.cpp
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #4 (Strings)/C_Compare.cpp
@@ -1,37 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    string x , y ;
-    cin >> x ;
-    cin >> y ;
-    int lx = strlen(x.c_str());
-    int ly = strlen(x.c_str());
-    int n;
-    if (lx > ly)
-    {
-        n=lx;
-    }
-    else if (lx < lx)
-    {
-        n = ly;
-    }
+    string x, y;
+    cin >> x >> y;
+
+    if (x < y)
+        cout << x << "\n";
     else
-    {
-        n = lx;
-    }
-    for (int i=0;i < n;i++)
-    {
-        if (x[i] < y[i])
-        {
-            cout << x;
-            return 0;
-        }
-        else if(x[i] > y[i])
-        {
-            cout << y;
-            return 0;
-        }
-    }
+        cout << y << "\n";
+
     return 0;
 }
