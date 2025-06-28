51d9971a2b9b1ee008b81805288220ae6a9634ce
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-06-28 21:14:04 +0600

    T_Matrix

---
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #3 (Arrays)/O_Fibonacci.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a = 0, b = 1, fib;
+
+    if (n == 1) {
+        cout << a;
+    } else if (n == 2) {
+        cout << b;
+    } else {
+        for (int i = 3; i <= n; i++) 
+        {
+            fib = a + b;
+            a = b;
+            b = fib;
+        }
+        cout << fib;
+    }
+
+    return 0;
+}
