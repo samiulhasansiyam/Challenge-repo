e0ddd9f5890eaf8bc98feb4bd4493340b68e8c1a
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-07-04 22:39:15 +0600

    R_Permutation_with_arrays

---
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #3 (Arrays)/R_Permutation_with_arrays.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    if (a == b)
+        cout << "yes";
+    else
+        cout << "no";
+
+    return 0;
+}
