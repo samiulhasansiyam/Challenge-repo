2b76287917e02134942f9ae67f581d2013533604
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-07-07 18:30:06 +0600

    E_Swap

---
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #5 (Functions)/E_Swap.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+void swap_two(int a, int b)
+{
+    cout << b << " " << a;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    swap_two(a, b);
+    return 0;
+}
