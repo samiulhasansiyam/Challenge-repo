2326279f0926fa7b8ee37e6e28676d37efb97b17
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-07-08 22:59:37 +0600

    B_Print

--- Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #5 (Functions)/B_Print.cpp
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #5 (Functions)/B_Print.cpp
@@ -1,9 +1,8 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-void print_to_n(int n)
+void printOneToN(int n)
 {
-    for (int i = 1; i <= n; i++)
+    for(int i=1;i<=n;i++)
     {
         cout << i;
         if (i < n)
@@ -11,14 +10,41 @@ void print_to_n(int n)
             cout << " ";
         }
     }
-    cout << "\n";
 }
-
 int main()
 {
     int n;
     cin >> n;
-
-    print_to_n(n);
+    printOneToN(n);
     return 0;
 }
+
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void print_to_n(int n)
+// {
+//     for (int i = 1; i <= n; i++)
+//     {
+//         cout << i;
+//         if (i < n)
+//         {
+//             cout << " ";
+//         }
+//     }
+//     cout << "\n";
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     print_to_n(n);
+//     return 0;
+// }
