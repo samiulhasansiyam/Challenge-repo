f051b821476133df71bb5f98a507628cf9d57767
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-07-08 20:48:30 +0600

    A_Add_Modify

--- Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #5 (Functions)/A_Add.cpp
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Sheet #5 (Functions)/A_Add.cpp
@@ -1,15 +1,32 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int sum(int x, int y)
+int addTwoNumbers(int a, int b)
 {
-    return x + y;
+    return a + b;
 }
 int main()
 {
-    int x, y;
-    cin >> x >> y;
-
-    cout << sum(x, y) << "\n";
+    int a, b;
+    cin >> a >> b;
+    cout << addTwoNumbers(a,b) << endl;
 
     return 0;
 }
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int sum(int x, int y)
+// {
+//     return x + y;
+// }
+// int main()
+// {
+//     int x, y;
+//     cin >> x >> y;
+
+//     cout << sum(x, y) << "\n";
+
+//     return 0;
+// }
