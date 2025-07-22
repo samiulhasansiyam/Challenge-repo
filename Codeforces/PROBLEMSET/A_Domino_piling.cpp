ba7455594eff7097fa62e8fbc8454cedfbbe5ba9
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-07-22 19:51:32 +0600

    A_Domino_piling

---
+++ Codeforces/PROBLEMSET/A_Domino_piling.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+
+    int totalSquares = m * n;
+    int maxDominos = totalSquares / 2;
+
+    cout << maxDominos << endl;
+
+    return 0;
+}
