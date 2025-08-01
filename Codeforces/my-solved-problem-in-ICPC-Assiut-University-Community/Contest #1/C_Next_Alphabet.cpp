aeda296f2b54156946641418268a264156a551ef
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-08-01 22:26:49 +0600

    C_Next_Alphabet

---
+++ Codeforces/my-solved-problem-in-ICPC-Assiut-University-Community/Contest #1/C_Next_Alphabet.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char n;
+    cin >> n;
+    if(n=='z')
+    {
+        cout << "a" << endl;
+    }
+    else
+    {
+    char next = n + 1;
+    cout << next << endl;
+    }
+    return 0;
+}
