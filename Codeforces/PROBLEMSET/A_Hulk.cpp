a6b033109712160cbceae261614c0908c23544fb
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-08-23 22:47:13 +0600

    not solved

---
+++ Codeforces/PROBLEMSET/A_Hulk.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //not solved
+    int n;
+    cin >> n;
+
+    if (n == 1)
+    {
+        cout << "I hate it" << endl;
+    }
+    else if (n == 2)
+    {
+        cout << "I hate that I love it" << endl;
+    }
+    else if (n == 3)
+    {
+        cout << "I hate that I love that I hate it" << endl;
+    }
+    return 0;
+}
