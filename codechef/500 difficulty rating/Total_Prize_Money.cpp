d81e0c055f522e875de7b9e31dcd170d2c96f161
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-09-14 22:13:04 +0600

    Total_Prize_Money

---
+++ codechef/500 difficulty rating/Total_Prize_Money.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int y, x;
+        cin >> y >> x;
+        cout << ((x * 90) + (y * 10)) << endl;
+    }
+    return 0;
+}
