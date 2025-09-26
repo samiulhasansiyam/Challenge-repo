191da1a67c7eddc62008589a7735f3d3f39ff2ab
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-09-26 22:44:31 +0600

    Chef_and_Wire_Frames

---
+++ codechef/500 difficulty rating/Chef_and_Wire_Frames.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, x;
+        cin >> n >> m >> x;
+        cout << (((n * 2) + (m * 2)) * x) << endl;
+    }
+    return 0;
+}
