c6d6b362257298e657f48a090395240c98a09466
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-10-01 23:27:04 +0600

    B_Coin_Games

---
+++ Codeforces/PROBLEMSET/B_Coin_Games.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        string s;
+        cin >> n >> s;
+        int cU = count(s.begin(), s.end(), 'U');
+        if (cU % 2 == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
