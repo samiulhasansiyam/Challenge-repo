307e5b4676c267f49b46403e1e531896c57230b5
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-10-09 19:32:37 +0600

    B_Double_Helix

---
+++ DIU CP Community/DCC Practice Contest - 1/B_Double_Helix.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char b;
+    cin >> b;
+    if (b == 'A')
+    {
+        cout << "T" << endl;
+    }
+    else if (b == 'G')
+    {
+        cout << "C" << endl;
+    }
+    else if (b == 'C')
+    {
+        cout << "G" << endl;
+    }
+    else if (b == 'T')
+    {
+        cout << "A" << endl;
+    }
+
+    return 0;
+}
