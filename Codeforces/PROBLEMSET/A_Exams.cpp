da09fd2f2c04d2050a9abdc089f358699eae663c
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-10-18 18:33:43 +0600

    A_Exams

---
+++ Codeforces/PROBLEMSET/A_Exams.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    if ((n * 3) <= k)
+    {
+        cout << 0 << endl;
+    }
+    else
+    {
+        cout << (n * 3) - k << endl;
+    }
+    return 0;
+}
