9c256e95db6bbbdf85612effe2e7cc3d15d6e399
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2025-10-19 23:43:22 +0600

    A_Vus_the_Cossack_and_a_Contest

---
+++ Codeforces/PROBLEMSET/A_Vus_the_Cossack_and_a_Contest.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    if (m < n || k < n)
+    {
+        cout << "No" << endl;
+    }
+    else
+    {
+        cout << "Yes" << endl;
+    }
+    return 0;
+}
