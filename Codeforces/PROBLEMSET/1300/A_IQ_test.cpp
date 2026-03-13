687326b44e78e68640b05b99f1d6eed883f95688
samiulhasansiyam <samiulhasansiyam@gmail.com>
2026-03-14 00:57:50 +0600

    A_IQ_test.cpp

---
+++ Codeforces/PROBLEMSET/1300/A_IQ_test.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i=0;i<n;i++)
+    {
+        cin >> a[i];
+    }
+
+    int evenCount=0,oddCount=0,evenIndex=0,oddIndex=0;
+    for (int i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            evenCount++;
+            evenIndex=i;
+        }
+        else
+        {
+            oddCount++;
+            oddIndex=i;
+        }
+    }
+    if (oddCount > evenCount)
+    {
+        cout << evenIndex +1 << endl;
+    }
+    else
+    {
+        cout << oddIndex +1 << endl;
+    }
+}
