a7b9d8bc1cf24432af234ec4f71ac92948b382d8
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2026-05-24 10:31:53 +0600

    A_Watermelon.cpp

---
+++ vjudge/Qurbanir Goru Contest 2026/A_Watermelon.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k;
+    cin >> k;
+    if (k > 2 && k % 2 == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
