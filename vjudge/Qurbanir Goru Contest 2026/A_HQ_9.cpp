53437b80ff4232a7da8a5c354cd021c5bb5aa7fa
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2026-05-26 22:34:02 +0600

    A_HQ_9.cpp

---
+++ vjudge/Qurbanir Goru Contest 2026/A_HQ_9.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string p;
+    cin >> p;
+
+    int c = 0;
+
+    for (int i = 0; i < p.length(); i++)
+    {
+        if (p[i] == 'H' || p[i] == 'Q' || p[i] == '9')
+        {
+            c = 1;
+            break;
+        }
+    }
+
+    if (c == 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
