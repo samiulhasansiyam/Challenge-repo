82a6efb70cdf3c9380aa316799941690f1fae4bc
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2026-06-01 15:33:09 +0600

    For_Loop.cpp

---
+++ hackerrank/Cpp/For_Loop.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    string arr[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    for (int i = a; i <= b; i++)
+    {
+        if (i >= 1 && i <= 9)
+        {
+            cout << arr[i - 1] << endl;
+        }
+        else
+        {
+            if (i % 2 == 0)
+            {
+                cout << "even" << endl;
+            }
+            else
+            {
+                cout << "odd" << endl;
+            }
+        }
+    }
+    return 0;
+}
