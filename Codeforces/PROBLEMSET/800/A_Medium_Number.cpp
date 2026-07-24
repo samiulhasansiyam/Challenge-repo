cc4524260279fd7b3741a7e3370e0a007359f8a4
Samiul Hasan Siyam <63056491+samiulhasansiyam@users.noreply.github.com>
2026-07-24 11:33:13 +0600

    A_Medium_Number.cpp

--- Codeforces/PROBLEMSET/800/A_Medium_Number.cpp
+++ Codeforces/PROBLEMSET/800/A_Medium_Number.cpp
@@ -1,46 +1,64 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int a, b, c;
+//         cin >> a >> b >> c;
+//         if (a < b && b > c)
+//         {
+//             if (a < c)
+//             {
+//                 cout << c << endl;
+//             }
+//             else
+//             {
+//                 cout << a << endl;
+//             }
+//         }
+//         else if (a < c && c > b)
+//         {
+//             if (a < b)
+//             {
+//                 cout << b << endl;
+//             }
+//             else
+//             {
+//                 cout << a << endl;
+//             }
+//         }
+//         else if (b < a && a > c)
+//         {
+//             if (c < b)
+//             {
+//                 cout << b << endl;
+//             }
+//             else
+//             {
+//                 cout << c << endl;
+//             }
+//         }
+//     }
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
     int t;
     cin >> t;
-    while (t--)
+    while(t--)
     {
-        int a, b, c;
+        int a,b,c;
         cin >> a >> b >> c;
-        if (a < b && b > c)
-        {
-            if (a < c)
-            {
-                cout << c << endl;
-            }
-            else
-            {
-                cout << a << endl;
-            }
-        }
-        else if (a < c && c > b)
-        {
-            if (a < b)
-            {
-                cout << b << endl;
-            }
-            else
-            {
-                cout << a << endl;
-            }
-        }
-        else if (b < a && a > c)
-        {
-            if (c < b)
-            {
-                cout << b << endl;
-            }
-            else
-            {
-                cout << c << endl;
-            }
-        }
+        int mi=min({a,b,c});
+        int mx=max({a,b,c});
+        int total=a+b+c;
+        cout << total-mi-mx << endl;
     }
     return 0;
 }
